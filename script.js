e687aa553fc957b954efa39b3f7f9a126f32c570
schmitt999 <schmitt.cristiano@escola.pr.gov.br>
2025-08-28 08:16:40 -0300

    script

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave'); 
 
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
 function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
+ const texto = document.querySelector('#entrada-de-texto').value;
+ const campoResultado =  document.querySelector('#resultado-palavrachave');
+ const palavrasChave = processaTexto(texto);
+
+ campoResultado.textContent = palavrasChave.join(", ");
 } 
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+    return palavras;
+}
